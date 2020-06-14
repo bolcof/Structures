71cd28c408fd1ccfda0b6f2c1c7b0fa5cbd287f3
bolcof <sanctum.crusade.ex@gmail.com>
2020-06-14 12:22:53 +0900

    感性

--- Assets/Script/Cube.cs
+++ Assets/Script/Cube.cs
@@ -6,9 +6,11 @@ using System.Linq;
 public class Cube : MonoBehaviour
 {
     [SerializeField] public int id, level;
-    [SerializeField] List<bool> isCovered;
+    [SerializeField] private List<bool> isCovered;
 
-    public void init (int id, int length) {
+    public List<bool> IsCovered => isCovered;
+
+    public void level0Init (int id, int length) {
         this.id = id;
         for (int i = 0; i < length; i++)
         {
@@ -19,17 +21,31 @@ public class Cube : MonoBehaviour
         }
     }
 
-    public void set (Vector3 pos, Vector3 size) {
+    public void set (int level, Vector3 pos, Vector3 size) {
+        this.level = level;
         this.gameObject.transform.localPosition = pos;
         this.gameObject.transform.localScale = size;
     }
 
+    public void setIsCoverd (List<bool> parentStats, int otherID)
+    {
+        for (int i = 0; i < parentStats.Count; i++) {
+            this.isCovered.Add(parentStats[i]);
+        }
+        this.isCovered[otherID] = true;
+    }
+
     public Cube col (Cube other) {
 
-        Vector3 thisPos = this.gameObject.transform.localPosition;
         Vector3 thisSize = this.gameObject.transform.localScale;
-        Vector3 otherPos = other.gameObject.transform.localPosition;
+        Vector3 thisPos = this.gameObject.transform.localPosition;
+        Vector3 thisPosMin = this.gameObject.transform.localPosition - thisSize / 2;
+        Vector3 thisPosMax = this.gameObject.transform.localPosition + thisSize / 2;
+
         Vector3 otherSize = other.gameObject.transform.localScale;
+        Vector3 otherPos = other.gameObject.transform.localPosition;
+        Vector3 otherPosMin = other.gameObject.transform.localPosition - otherSize / 2;
+        Vector3 otherPosMax = other.gameObject.transform.localPosition + otherSize / 2;
 
         bool hitX = Mathf.Abs(otherPos.x - thisPos.x) < (otherSize.x + thisSize.x) / 2;
         bool hitY = Mathf.Abs(otherPos.y - thisPos.y) < (otherSize.y + thisSize.y) / 2;
@@ -39,35 +55,40 @@ public class Cube : MonoBehaviour
 
         if(isHit){
             Vector3 newCubeSize = new Vector3(
-                ((otherSize.x + thisSize.x) / 2) - Mathf.Abs(otherPos.x - thisPos.x),
-                ((otherSize.y + thisSize.y) / 2) - Mathf.Abs(otherPos.y - thisPos.y),
-                ((otherSize.z + thisSize.z) / 2) - Mathf.Abs(otherPos.z - thisPos.z)
+                Mathf.Min(thisPosMax.x, otherPosMax.x) - Mathf.Max(thisPosMin.x, otherPosMin.x),
+                Mathf.Min(thisPosMax.y, otherPosMax.y) - Mathf.Max(thisPosMin.y, otherPosMin.y),
+                Mathf.Min(thisPosMax.z, otherPosMax.z) - Mathf.Max(thisPosMin.z, otherPosMin.z)
             );
 
-            Vector3 newCubePos;
-            if(otherPos.x - thisPos.x >= 0){
-                newCubePos.x = (thisSize.x - newCubeSize.x) / 2;
-            }else{
-                newCubePos.x = - (thisSize.x - newCubeSize.x) / 2;
-            }
-
-            if(otherPos.y - thisPos.y >= 0){
-                newCubePos.y = (thisSize.y - newCubeSize.y) / 2;
-            }else{
-                newCubePos.y = - (thisSize.y - newCubeSize.y) / 2;
-            }
-
-            if(otherPos.z - thisPos.z >= 0){
-                newCubePos.z = (thisSize.z - newCubeSize.z) / 2;
-            }else{
-                newCubePos.z = - (thisSize.z - newCubeSize.z) / 2;
-            }
+            Vector3 newCubePos = new Vector3(
+                Mathf.Max(thisPosMin.x, otherPosMin.x) + newCubeSize.x / 2,
+                Mathf.Max(thisPosMin.y, otherPosMin.y) + newCubeSize.y / 2,
+                Mathf.Max(thisPosMin.z, otherPosMin.z) + newCubeSize.z / 2
+                );
+            
 
             GameObject newCube = Instantiate(Resources.Load("CubeOrigin"), this.gameObject.transform.parent) as GameObject;
-            newCube.GetComponent<Cube>().set(newCubePos, newCubeSize);
+            newCube.GetComponent<Cube>().set(this.level+1, newCubePos, newCubeSize);
             return newCube.GetComponent<Cube>();
         }else{
             return null;
         }
+    }
+
+    public float returnSurface()
+    {
+        Vector3 thisScale = this.gameObject.transform.localScale;
+
+        return
+            (thisScale.x * thisScale.y * 2) +
+            (thisScale.y * thisScale.z * 2) +
+            (thisScale.z * thisScale.x * 2);
+    }
+
+    public float returnVolume()
+    {
+        Vector3 thisScale = this.gameObject.transform.localScale;
+
+        return thisScale.x * thisScale.y * thisScale.z;
     }
 }
--- Assets/Script/Structure.cs
+++ Assets/Script/Structure.cs
@@ -12,9 +12,10 @@ public class Structure : MonoBehaviour
         nowSurface;
 
     [SerializeField] private List<Cube> cubes;
-    [SerializeField] private List<Cube> level0Cubes;
+    [SerializeField] public List<Cube> level0Cubes;
 
     [SerializeField] private int maxLevel = 0;
+    [SerializeField] private int count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -26,19 +27,29 @@ public class Structure : MonoBehaviour
         }
         Debug.Log("cube count:" + cubes.Count.ToString());
 
-        for (int i = 0; i < cubes.Count; i++) {
-            cubes[i].init(i, cubes.Count);
+        for (count = 0; count < cubes.Count; count++) {
+            cubes[count].level0Init(count, cubes.Count);
         }
 
-        makeNextLevel(0);
+        makeLevel1();
 
-        // while
-    }
+        bool continued = true;
+        for(int i = 1; continued; i++)
+        {
+            continued = makeNextLevel(i);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        nowVolume = nowSurface = 0.0f;
+        for (int i = 0; i <= maxLevel; i++)
+        {
+            List<Cube> matchCubes = cubes.Where(C => C.level == i).ToList();
+            for (int j = 0; j < matchCubes.Count; j++)
+            {
+                int seed = i % 2 == 0 ? 1 : -1;
+                nowSurface += matchCubes[j].returnSurface() * seed;
+                nowVolume += matchCubes[j].returnVolume() * seed;
+            }
+        }
     }
 
     bool makeNextLevel(int level){
@@ -46,20 +57,53 @@ public class Structure : MonoBehaviour
         Debug.Log("level:" + level.ToString() + " : " + matchCubes.Count.ToString());
         if (matchCubes.Count != 0)
         {
-            for (int i = 0; i < matchCubes.Count; i++){
-                for (int j = 0; j < level0Cubes.Count; j++)
+            for (int i = 0; i < matchCubes.Count; i++)
+            {
+                for (int j = searchMax(matchCubes[i].IsCovered); j < level0Cubes.Count; j++)
                 {
-                    Cube newCube = matchCubes[i].col(level0Cubes[i]);
-                    if(newCube != null){
-                        cubes.Add(newCube);
+                    if (!matchCubes[i].IsCovered[j])
+                    {
+                        Cube newCube = matchCubes[i].col(level0Cubes[j]);
+                        if (newCube != null)
+                        {
+                            cubes.Add(newCube);
+                            newCube.setIsCoverd(matchCubes[i].IsCovered, j);
+                        }
                     }
                 }
             }
             return true;
         }else{
             Debug.Log("generate complete : " + level.ToString());
+            maxLevel = level - 1;
             return false;
         }
+    }
+
+    void makeLevel1()
+    {
+        for (int i = 0; i < level0Cubes.Count; i++)
+        {
+            for (int j = i + 1; j < level0Cubes.Count; j++)
+            {
+                Cube newCube = level0Cubes[i].col(level0Cubes[j]);
+                if (newCube != null)
+                {
+                    cubes.Add(newCube);
+                    newCube.setIsCoverd(level0Cubes[i].IsCovered, j);
+                }
+            }
+        }
+    }
+
+    int searchMax (List<bool> source)
+    {
+        int max = -1;
+        for(int i = 0; i < source.Count; i++)
+        {
+            if (source[i]) { max = i; }
+        }
+        return max;
     }
 
     private void outputBoth () {
